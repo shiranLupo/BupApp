77e3b30ca680a95678524519ad22289f9c15b370
shiranLupo <shiran.lupo@gmail.com>
2020-07-12 18:24:03 +0300

    same

--- appFiles/_clientMain.cpp
+++ appFiles/_clientMain.cpp
@@ -9,16 +9,10 @@ using namespace utils;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"test main"<<endl;
-    
     BupApp::appClient pc(argc, argv);
     pc.init();
     pc.working();
     pc.disconnect();
     
-//     string key = getTxtFromFile("/home/shiranlupo/.ssh/id_rsa.pub");
-//     cout<<key << key.size()<<endl;
-//    utils::addStrToFile(key, "/home/shiranlupo/.ssh/authorized_keys");
-
     return 0;
 }
